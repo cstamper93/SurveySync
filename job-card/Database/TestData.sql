3c5a5a9969cafb47c0d6e3a9b75a2bf5121c7878
Corey Stamper <cstamper93@gmail.com>
2025-07-23 23:11:56 -0400

    began inputting test data

--- job-card/Database/TestData.sql
+++ job-card/Database/TestData.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
 
-
+INSERT INTO job_card (intake_date, intake_time, marked_lines_length,
+job_description, job_status, complete_by_date) VALUES ('2025-07-23', '2025-070-23 12:00:00')
 
 COMMIT TRANSACTION;
