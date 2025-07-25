b255da4eeabb742d0e42f7a146aff7b1970356ce
Corey Stamper <cstamper93@gmail.com>
2025-07-25 00:04:38 -0400

    Completed debugging test data. Tested endpoint in postman. It worked

--- job-card/Database/create.sh
+++ job-card/Database/create.sh
@@ -6,4 +6,4 @@ DATABASE=dzt_job_card_db
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/dzt_job_card_script.sql"
-#psql -U postgres -d $DATABASE -f "$BASEDIR/TestData.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/TestData.sql"
