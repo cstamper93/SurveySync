f74184bb3f80286693483d1f925612b0e47af625
Corey Stamper <cstamper93@gmail.com>
2025-08-10 00:49:08 -0400

    Added email model

---
+++ job-card/src/main/java/com/dzt/job_card/Models/ClientEmail.java
@@ -0,0 +1,23 @@
+package com.dzt.job_card.Models;
+
+public class ClientEmail {
+
+    private int emailId;
+    private String emailAddress;
+
+    public int getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(int emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
--- job-card/src/main/java/com/dzt/job_card/Models/ClientPhone.java
+++ job-card/src/main/java/com/dzt/job_card/Models/ClientPhone.java
@@ -5,4 +5,28 @@ public class ClientPhone {
     private int phoneId;
     private String phoneNumber; // make enum?
     private String phoneType;
+
+    public int getPhoneId() {
+        return phoneId;
+    }
+
+    public void setPhoneId(int phoneId) {
+        this.phoneId = phoneId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneType() {
+        return phoneType;
+    }
+
+    public void setPhoneType(String phoneType) {
+        this.phoneType = phoneType;
+    }
 }
