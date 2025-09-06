8aa353002b9b4df578632f4b26fe67adcd197518
Corey Stamper <cstamper93@gmail.com>
2025-09-06 00:54:57 -0400

    Working on needs research prospect display

---
+++ job-manager-client/src/components/DisplayNeedsResearchProspects.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="needs-research-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NeedsResearchDisplay',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    needsResearchProspect: Object
+  }
+
+}
+</script>
+
+<style>
+
+</style>
