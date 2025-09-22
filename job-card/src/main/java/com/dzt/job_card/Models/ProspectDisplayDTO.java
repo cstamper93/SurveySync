e434f1d3888733dce13fb057a853cb347fc0cfbd
Corey Stamper <cstamper93@gmail.com>
2025-09-21 23:05:28 -0400

    working on adding java dto for prospect display on client side

---
+++ job-card/src/main/java/com/dzt/job_card/Models/ProspectDisplayDTO.java
@@ -0,0 +1,17 @@
+package com.dzt.job_card.Models;
+
+import java.util.Date;
+
+public class ProspectDisplayDTO {
+
+    private int prospectId;
+    private int activeJobId;
+    private String clientFirstName;
+    private String clientLastName;
+    private String jobAddress;
+    private String jobTown;
+    private String jobCounty;
+    private Date intakeDate;
+    private String jobType;
+
+}
